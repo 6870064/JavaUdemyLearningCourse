2d958b90bd9e1d580034084d29d83d44fe612602
dzmitry.viachaslavau <6870064@gmail.com>
2022-07-25 13:09:05 +0300

    fix of Matrix

--- src/com/examclouds/OOPTasks/MatrixClassTask/MatrixTask.java
+++ src/com/examclouds/OOPTasks/MatrixClassTask/MatrixTask.java
@@ -65,9 +65,9 @@ public class MatrixTask {
     }
 
     public void displayMatrix() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(mainMatrix[i][j] + " ");
+        for(int i = 0; i < this.n; i++) {
+            for(int j = 0; j < this.m; j++) {
+                System.out.print(this.mainMatrix[i][j] + " ");
             }
             System.out.println();
         }
