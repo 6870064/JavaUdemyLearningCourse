ca75280d32e58a9faebbaa7d71b8923e56f90acd
dzmitry.viachaslavau <6870064@gmail.com>
2022-08-05 13:11:43 +0300

    feature/stask

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import com.company.StackQueue.Passenger;
+
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        passengerProcessing();
     }
+
+private static void passengerProcessing(){
+        Stack<Passenger> bus = new Stack<>();
+        Passenger passenger = new Passenger("Katerina", "Ivanova");
+        bus.push(new  Passenger("Alex", "Vasko"));
+        bus.push(new Passenger("Viktor", "Mikhailov"));
+        bus.push(new Passenger("Dmitry", "Petrov"));
+        bus.push(passenger);
+        bus.push(new Passenger("Ivan", "Ivanov"));
+
+    System.out.println("Passenger found at position "+bus.search(passenger));
+
+    System.out.println("The last passenger is: " +bus.peek());
+    while(!bus.empty()){
+        System.out.println(" Passenger : "+bus.pop());
+    }
+}
 }
---
+++ src/com/company/StackQueue/StackQueueLauncher.java
@@ -0,0 +1,7 @@
+package com.company.StackQueue;
+
+public class StackQueueLauncher {
+    public static void main(String[] args) {
+
+    }
+}
