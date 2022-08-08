d9827c3de2a37b32bfdf61a655c22194210b1dfd
dzmitry.viachaslavau <6870064@gmail.com>
2022-08-08 16:03:11 +0300

    feature/threemap runner

---
+++ src/com/company/javaio/IOMain.java
@@ -0,0 +1,7 @@
+package com.company.javaio;
+
+public class IOMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/javaio/TreeMapRunner.java
@@ -0,0 +1,4 @@
+package com.company.javaio;
+
+public class TreeMapRunner {
+}
