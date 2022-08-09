f72f2bce520da212e897875f05073b926515cc2c
dzmitry.viachaslavau <6870064@gmail.com>
2022-08-09 23:04:05 +0300

    feature/ByteStream

--- src/com/company/javaio/IOMain.java
+++ src/com/company/javaio/IOMain.java
@@ -1,21 +1,45 @@
 package com.company.javaio;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.NavigableMap;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class IOMain {
+
+    private static final String FILE_MAME = "GradeBook.txt";
     public static void main(String[] args) throws IOException {
         NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades = TreeMapRunner.createGrades();
-        readFile(grades);
+//        readFile(grades);
+//        readFile();
+
+        Formatter formatter = new Formatter("BankAccounts.txt");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter valid clientId, ClientName, Client surname, account balance");
+        int i = 0;
+        while (i < 3) {
+            try {
+                formatter.format("%d, %s, %s, %.2f%n", scanner.nextInt(), scanner.next(), scanner.next(), scanner.nextFloat());
+                i++;
+            } catch (InputMismatchException e) {
+                System.out.println("Input is incorrect.Please try again");
+                scanner.nextLine();
+            }
+        }
+        formatter.close();
+
+    }
+
+    private static void readFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(FILE_MAME));
+        String c;
+        while ((c = reader.readLine()) != null) {
+            System.out.println(c);
+        }
     }
 
     private static void readFile(NavigableMap<AverageStudentGrade, Set<SubjectGrade>> grades) throws IOException {
-        FileWriter writer = null;
-        try {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_MAME))) {
 
-            writer = new FileWriter("GradeBook.txt");
             for (AverageStudentGrade gradeKey : grades.keySet()) {
                 writer.write("=============================\n");
                 writer.write("Student: " + gradeKey.getName() + " Average grade: " + gradeKey.getAverageGrade() + "\n");
@@ -23,8 +47,6 @@ public class IOMain {
                     writer.write("Subject: " + grade.getSubject() + " Grade: " + grade.getGrade() + "\n");
                 }
             }
-        } finally {
-                writer.close();
         }
     }
 }
