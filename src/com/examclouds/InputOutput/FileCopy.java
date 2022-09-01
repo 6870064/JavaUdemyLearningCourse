bf6bcc032bb311b72977479b0c9202d886772f45
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-01 19:47:08 +0300

    feature/FileInputStream и FileOutputStream

---
+++ src/com/examclouds/InputOutput/FileCopy.java
@@ -0,0 +1,32 @@
+package com.examclouds.InputOutput;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+    final static String INPUT_FILE_NAME = "TestInputDoc.txt";
+    final static String OUTPUT_FILE_NAME = "TestOutputDoc.txt";
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileIn = null;
+        FileOutputStream fileOut = null;
+
+        try {
+            fileIn = new FileInputStream(INPUT_FILE_NAME);
+            fileOut = new FileOutputStream(OUTPUT_FILE_NAME);
+
+            int a;
+            while ((a = fileIn.read()) != -1){
+                fileOut.write(a);
+            }
+        } finally {
+            if (fileIn !=null) {
+                fileIn.close();
+            } if (fileOut !=null) {
+                fileOut.close();
+            }
+        }
+    }
+}
