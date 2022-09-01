8a6c3906ed4fc3f4081d11118ac8707cc67b077a
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-01 20:19:53 +0300

    FileInputStream и FileOutputStream

--- src/com/examclouds/InputOutput/FileInputOutputStreamDemo.java
+++ src/com/examclouds/InputOutput/FileInputOutputStreamDemo.java
@@ -1,4 +1,31 @@
 package com.examclouds.InputOutput;
 
+import java.io.*;
+
 public class FileInputOutputStreamDemo {
+
+    private static final String FILE_NAME = "a.txt";
+    public static void main(String[] args) {
+        try {
+            char [] symbols = {'a', 'b', 'c'};
+            OutputStream output = new FileOutputStream(FILE_NAME);
+            for (int i = 0; i < symbols.length; i++){
+                // Запись каждого символа в текстовый файл
+                output.write(symbols[i]);
+            }
+            output.close();
+
+            InputStream input = new FileInputStream(FILE_NAME);
+            int size = input.available();
+
+            for (int i = 0; i < size; i++) {
+                //Чтение текстового файла посимвольно
+                System.out.println((char) input.read() + " ");
+            }
+            input.close();
+
+        } catch (IOException e) {
+            System.out.println("Exception");
+        }
+    }
 }
