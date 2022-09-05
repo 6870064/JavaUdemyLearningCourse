c2641f2610f0e4c804fbf9ddd47a7c03c4de9ae6
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-05 12:25:20 +0300

    FileOutputStreamDemo

---
+++ src/com/examclouds/InputOutput/FileInputOutputStreamIntDemo.java
@@ -0,0 +1,32 @@
+package com.examclouds.InputOutput;
+
+import java.io.*;
+
+public class FileInputOutputStreamIntDemo {
+
+    private static final String FILE_NAME = "integer_list.txt";
+
+    public static void main(String[] args) {
+        try {
+            char[] symbols = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            OutputStream output = new FileOutputStream(FILE_NAME);
+            for (int i = 0; i < symbols.length; i++) {
+                // Запись каждого числа в текстовый файл
+                output.write(symbols[i]);
+            }
+            output.close();
+
+            InputStream input = new FileInputStream(FILE_NAME);
+            int size = input.available();
+
+            for (int i = 0; i < size; i++) {
+                //Чтение текстового файла посимвольно
+                System.out.println((char) input.read() + " ");
+            }
+            input.close();
+
+        } catch (IOException e) {
+            System.out.println("Exception");
+        }
+    }
+}
