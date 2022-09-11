2d30cca6d957964b7917916396cb0b55e0d7ffea
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-11 23:47:54 +0300

    Class Scanner using

---
+++ src/com/examclouds/InputOutput/BufferedReaderWrapperDemo.java
@@ -0,0 +1,24 @@
+package com.examclouds.InputOutput;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BufferedReaderWrapperDemo {
+    public static void main(String[] args) {
+        String s1 = "Первая строка";
+        String s2 = "Вторя строка";
+
+        try (PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("printWriterWrapper.txt")))) {
+            printWriter.println(s1);
+            int i = 2;
+            printWriter.printf("Квадрат числа %d %n", i, i * i);
+            printWriter.write(s2);
+            printWriter.write("\nEnd of the line");
+            System.out.println("Запись в файл произведена");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
