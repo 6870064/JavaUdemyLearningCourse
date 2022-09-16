b055bc4206116172ff29cc39d833d13b16af201b
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-16 23:36:39 +0300

    feature/compare

---
+++ src/com/examclouds/iv_java_operations/BooleanLogic1.java
@@ -0,0 +1,14 @@
+package com.examclouds.iv_java_operations;
+
+public class BooleanLogic1 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = a | b;
+        System.out.println(c);
+        boolean d = a & b;
+        System.out.println(d);
+
+
+    }
+}
---
+++ src/com/examclouds/iv_java_operations/CharInc.java
@@ -0,0 +1,10 @@
+package com.examclouds.iv_java_operations;
+
+public class CharInc {
+    public static void main(String[] args) {
+        char ch = 'X';
+        System.out.println("ch содержит " +ch);
+        ch++;
+        System.out.println("Новое значение ch: " +ch);
+    }
+}
