660bc8ca3183ef92df0a971b0e3c2c02bdc20bcb
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-19 16:03:08 +0300

    feature/StringCompare

---
+++ src/com/examclouds/iv_java_operations/StringExample5.java
@@ -0,0 +1,15 @@
+package com.examclouds.iv_java_operations;
+
+import java.util.Objects;
+
+public class StringExample5 {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hi";
+
+        System.out.println(str1.equals(str2));
+        System.out.println(Objects.equals(str1, str2));
+
+        System.out.println("Длина строки : " +str1.length());
+    }
+}
