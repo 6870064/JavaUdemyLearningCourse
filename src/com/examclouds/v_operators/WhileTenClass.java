5df514f06011579b2f6eb99d687fc3f9068f7c4d
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-20 23:04:23 +0300

    feature/WhileTenClass

---
+++ src/com/examclouds/v_operators/WhileTenClass.java
@@ -0,0 +1,11 @@
+package com.examclouds.v_operators;
+
+public class c {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10){
+            System.out.println(String.format("Task%d",i));
+            i++;
+        }
+    }
+}
