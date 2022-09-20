68e86d0f260cf7de35e95c691c2bdf0c4501cab1
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-20 23:42:15 +0300

    feature/chooseDayOfTheWeek

---
+++ src/com/examclouds/v_operators/DaysOfTheWeeksSecondClass.java
@@ -0,0 +1,38 @@
+package com.examclouds.v_operators;
+
+public class DaysOfTheWeeksSecondClass {
+    public static void main(String[] args) {
+        chooseDayOfTheWeek(1);
+        chooseDayOfTheWeek(3);
+        chooseDayOfTheWeek(6);
+    }
+
+    public static void chooseDayOfTheWeek(int dayNumber) {
+        String dayOfTheWeek = "Понедельник";
+        switch (dayNumber) {
+            case 1:
+                dayOfTheWeek = "Понедельник";
+                break;
+            case 2:
+                dayOfTheWeek = "Вторник";
+                break;
+            case 3:
+                dayOfTheWeek = "Среда";
+                break;
+            case 4:
+                dayOfTheWeek = "Четверг";
+                break;
+            case 5:
+                dayOfTheWeek = "Пятница";
+            case 6:
+                dayOfTheWeek = "Суббота";
+                break;
+            case 7:
+                dayOfTheWeek = "Воскресенье";
+                break;
+            default:
+                break;
+        }
+        System.out.println(String.format("%s - день недели под номером %d", dayOfTheWeek, dayNumber));
+    }
+}
