e1574b15e26698190813abedb623daafb021f628
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-21 01:22:03 +0300

    feature/Factorial

---
+++ src/com/examclouds/v_operators/FactorialClass.java
@@ -0,0 +1,16 @@
+package com.examclouds.v_operators;
+
+import java.util.Random;
+
+public class FactorialClass {
+    public static void main(String[] args) {
+        int factorial = 1;
+        Random random = new Random();
+        int i = random.nextInt(6);
+
+        for (int a = 1; a <= i; a++) {
+            factorial = factorial * a;
+        }
+        System.out.println(String.format("Факториал %s равен %s", i, factorial));
+    }
+}
