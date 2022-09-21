b30a7822131680a2acc983680736dbb880287c2b
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-21 14:34:56 +0300

    feature/CharsDemoClass

---
+++ src/com/examclouds/v_operators/CharsDemoClass.java
@@ -0,0 +1,10 @@
+package com.examclouds.v_operators;
+
+public class CharsDemoClass {
+    public static void main(String[] args) {
+        char symbol = 'h';
+        for (int i = 0; i < 5; i++){
+            System.out.println(symbol++);
+        }
+    }
+}
