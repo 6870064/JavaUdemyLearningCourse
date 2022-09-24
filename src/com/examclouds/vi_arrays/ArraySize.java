ca53f6e4645337453dbccbfeae4573d37f87177d
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-24 22:45:46 +0300

    feature/arrayLength

---
+++ src/com/examclouds/vi_arrays/ArraySize.java
@@ -0,0 +1,11 @@
+package com.examclouds.vi_arrays;
+
+public class ArraySize {
+    public static void main(String[] args) {
+        int [] array1 = {1,2,3,4,5,6,7,8,9,10};
+        int [][] array2 = {{1,1,1}, {2,2,2},{3,3,3,3}};
+        System.out.println("Размера массива array1 = " +array1.length);
+        System.out.println("Размер массива array2 = " +array2.length);
+        System.out.println("Размер 1-й строки массива array2 = " +array2[2].length);
+    }
+}
