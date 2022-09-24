8e7dfc797063fbc69dbff570db5f7eb92a28a936
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-25 00:09:05 +0300

    feature/arraysStringTask

---
+++ src/com/examclouds/vi_arrays/tasks/arraysStringTask.java
@@ -0,0 +1,17 @@
+package com.examclouds.vi_arrays.tasks;
+
+public class arraysStringTask {
+    public static void main(String[] args) {
+        String[] weekArray = new String[7];
+            weekArray[0] = "Monday";
+            weekArray[1] = "Tuesday";
+            weekArray[2] = "Wednesday";
+            weekArray[3] = "Thursday";
+            weekArray[4] = "Friday";
+            weekArray[5] = "Saturday";
+            weekArray[6] = "Sunday";
+
+        System.out.println(weekArray[6]);
+
+    }
+}
