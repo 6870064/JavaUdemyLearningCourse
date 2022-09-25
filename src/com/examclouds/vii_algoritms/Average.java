ddc3eda306eca62d904d1203c72f4dceafa32364
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-25 22:02:44 +0300

    feature/Average

--- src/com/examclouds/vii_algoritms/Average.java
+++ src/com/examclouds/vii_algoritms/Average.java
@@ -2,6 +2,19 @@ package com.examclouds.vii_algoritms;
 
 public class Average {
     public static void main(String[] args) {
-        
+        double[] myArray = new double[10];
+        double result = 0;
+
+        for (double d : myArray) {
+            d = randomGenerator();
+            result += d;
+            System.out.print(d + " ");
+        }
+        System.out.println();
+        System.out.println(String.format("Среднее арифметическое массива равно %.2f", result / myArray.length));
+    }
+
+    public static double randomGenerator() {
+        return Math.random() * 100;
     }
 }
