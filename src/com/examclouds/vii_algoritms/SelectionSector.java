6a84978f9042dd4cd250b73d9ce8999452d5145c
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-27 12:14:32 +0300

    feature/SelectionSorter

---
+++ src/com/examclouds/vii_algoritms/SelectionSector.java
@@ -0,0 +1,43 @@
+package com.examclouds.vii_algoritms;
+
+import java.util.Arrays;
+
+public class SelectionSector {
+    public static void main(String[] args) {
+        int[] myArray = arrayGenerator(10);
+        sortArray(myArray);
+
+        int[] myArray1 = arrayGenerator(11);
+        sortArray(myArray1);
+    }
+
+    public static void sortArray(int[] array) {
+        System.out.print(Arrays.toString(array) + " => ");
+        for (int i = 0; i < array.length; i++) {
+            int pos = i;
+            int min = array[i];
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            array[pos] = array[i];
+            array[i] = min;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int[] arrayGenerator(int size) {
+        int[] myArray = new int[size];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = randomGenerator();
+        }
+        return myArray;
+    }
+
+    public static int randomGenerator() {
+        return (int) (Math.random() * 100);
+    }
+}
