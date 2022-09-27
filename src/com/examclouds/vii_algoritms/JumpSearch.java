210de00236fac6dbb6f5878734926190ee0ca4dd
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-27 15:48:24 +0300

    feature/Jump Search

---
+++ src/com/examclouds/vii_algoritms/JumpSearch.java
@@ -0,0 +1,35 @@
+package com.examclouds.vii_algoritms;
+
+public class JumpSearch {
+    public static void main(String[] args) {
+        int[] myArray = {12, 76, 98, 32, 65, 11, 49, 50};
+        int a = 49;
+        System.out.println(jumpSearch(myArray, a));
+
+    }
+
+    public static int jumpSearch(int[] array, int elementToSearch) {
+        int arrayLength = array.length;
+        int jumpStep = (int) Math.sqrt(array.length);
+        int previousStep = 0;
+
+        while (array[Math.min(jumpStep, arrayLength) - 1] < elementToSearch) {
+            previousStep = jumpStep;
+            jumpStep += (int) (Math.sqrt(arrayLength));
+            if (previousStep >= arrayLength) {
+                return -1;
+            }
+        }
+        while (array[previousStep] < elementToSearch) {
+            previousStep++;
+            if (previousStep == Math.min(jumpStep, arrayLength)) {
+                return -1;
+            }
+        }
+
+        if (array[previousStep] == elementToSearch) {
+            return previousStep;
+        }
+        return -1;
+    }
+}
