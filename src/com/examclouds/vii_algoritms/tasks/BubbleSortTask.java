9e144aacefd85727dab90b8ba0d30c77de6959f3
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-27 23:46:51 +0300

    feature/Bubble Sort Task

--- src/com/examclouds/vii_algoritms/tasks/BubbleSortTask.java
+++ src/com/examclouds/vii_algoritms/tasks/BubbleSortTask.java
@@ -1,21 +1,39 @@
 package com.examclouds.vii_algoritms.tasks;
 
+import java.util.Arrays;
+
 public class BubbleSortTask {
     public static void main(String[] args) {
         int[] myArray = arrayGenerator(12);
+        bubbleSort(myArray);
 
+        int[] myArray1 = arrayGenerator(14);
+        bubbleSort(myArray1);
+    }
+
+    public static void bubbleSort(int[] array){
+        System.out.print(Arrays.toString(array)+ " => ");
+        for (int i =0; i < array.length -1; i++){
+            for (int j = array.length -1; j > i; j--){
+                if (array[j - 1] > array[j]) {
+                    int tmp = array[j - 1];
+                    array[j-1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 
     public static int[] arrayGenerator(int size){
         int[] myArray = new int[size];
-        for (int i = 0; i < myArray.length; i++){
+        for(int i = 0; i < myArray.length; i++){
             myArray[i] = randomGenerator();
-            System.out.print(myArray[i]+ " ");
         }
         return myArray;
     }
 
-    public static int randomGenerator(){
+   public static int randomGenerator(){
         return (int) (Math.random() * 100);
-    }
+   }
 }
