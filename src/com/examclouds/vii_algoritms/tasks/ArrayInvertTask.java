41063065b7db921dfa6266da8e13776f6d016731
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-28 12:47:33 +0300

    feature/ArrayInvert Task

--- src/com/examclouds/vii_algoritms/tasks/ArrayInvertTask.java
+++ src/com/examclouds/vii_algoritms/tasks/ArrayInvertTask.java
@@ -2,10 +2,23 @@ package com.examclouds.vii_algoritms.tasks;
 
 import java.util.Arrays;
 
-public class ArraySortTask {
+public class ArrayInvertTask {
     public static void main(String[] args) {
         int[] myArray = arrayGenerator(12);
         arrayInvert(myArray);
+        int[] myArray1 = arrayGenerator(11);
+        arrayInvert(myArray1);
+
+    }
+
+    public static void arrayInvert(int[] array) {
+        System.out.println(Arrays.toString(array) + " => ");
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
+        System.out.println(Arrays.toString(array));
     }
 
     public static int[] arrayGenerator(int size) {
@@ -14,16 +27,6 @@ public class ArraySortTask {
             myArray[i] = randomGenerator();
         }
         return myArray;
-    }
-
-    public static void arrayInvert(int[] array) {
-        System.out.print(Arrays.toString(array) + " => ");
-        for (int i = 0; i < array.length / 2; i++) {
-            int tmp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = tmp;
-        }
-        System.out.println(Arrays.toString(array));
     }
 
     public static int randomGenerator() {
