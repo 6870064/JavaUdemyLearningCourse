b5f9c262ab837fe3f7aeaf83748ddebae3f49432
dzmitry.viachaslavau <6870064@gmail.com>
2022-09-28 13:30:08 +0300

    feature/selection Sort Array task

--- src/com/examclouds/vii_algoritms/tasks/SelectionSectorTask.java
+++ src/com/examclouds/vii_algoritms/tasks/SelectionSectorTask.java
@@ -2,20 +2,29 @@ package com.examclouds.vii_algoritms.tasks;
 
 import java.util.Arrays;
 
-public class ArrayInvertTask {
+public class SelectionSectorTask {
     public static void main(String[] args) {
         int[] myArray = arrayGenerator(12);
-        arrayInvert(myArray);
-        int[] myArray1 = arrayGenerator(11);
-        arrayInvert(myArray1);
+        selectionArraySort(myArray);
+
+        int[] myArray1 = arrayGenerator(15);
+        selectionArraySort(myArray1);
     }
 
-    public static void arrayInvert(int[] array) {
+    public static void selectionArraySort(int[] array) {
         System.out.println(Arrays.toString(array) + " => ");
-        for (int i = 0; i < array.length / 2; i++) {
-            int tmp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i - 1] = tmp;
+        for (int i = 0; i < array.length; i++) {
+            int pos = i;
+            int min = array[i];
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            array[pos] = array[i];
+            array[i] = min;
         }
         System.out.println(Arrays.toString(array));
     }
