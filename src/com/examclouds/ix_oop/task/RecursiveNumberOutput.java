6fd616c264b15bfc60ef7db54bb8a285a4af66c1
dzmitry.viachaslavau <6870064@gmail.com>
2022-10-04 23:02:32 +0300

    feature/print numbers

--- src/com/examclouds/ix_oop/task/RecursiveNumberOutput.java
+++ src/com/examclouds/ix_oop/task/RecursiveNumberOutput.java
@@ -2,10 +2,18 @@ package com.examclouds.ix_oop.task;
 
 public class RecursiveNumberOutput {
     public static void main(String[] args) {
-
-
-
+        printNumbers(12, 24);
     }
 
-
+    public static void printNumbers(int a, int b) {
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                System.out.println(i + " ");
+            }
+        } else {
+            for (int i = a; i >= b; i++) {
+                System.out.println(i + " ");
+            }
+        }
+    }
 }
