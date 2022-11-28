26ab180aaa594b63e59b39a66e41d271d7f2db00
dzmitry.viachaslavau <6870064@gmail.com>
2022-11-29 00:51:45 +0300

    ReentrantLock

--- src/main/java/com/javcode/thread/synch/locks/WaitingQueue.java
+++ src/main/java/com/javcode/thread/synch/locks/WaitingQueue.java
@@ -1,12 +1,41 @@
 package com.javcode.thread.synch.locks;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static com.javcode.thread.ColorScheme.GREEN;
+import static com.javcode.thread.ColorScheme.RED;
 
 public class WaitingQueue {
 
     public static void main(String[] args) throws InterruptedException {
+        int operators = 5;
+        int customers = 21;
+
+        SemaphoredServiceDesk serviceDesk = new SemaphoredServiceDesk(operators, customers);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        IntStream.range(0, customers)
+                .forEach(client -> executorService.submit(()-> {
+           serviceDesk.connect();
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(GREEN+ "Number of connected customers " + serviceDesk.getCustomersConnected());
+                    System.out.println(RED + "Number of customers in a queue " +serviceDesk.getCustomersWaiting());
+        }));
+
+        executorService.shutdown();
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
 
     }
 
@@ -46,6 +75,10 @@ public class WaitingQueue {
 
         private int getCustomersConnected() {
             return connectedCustomers.get();
+        }
+
+        private int getCustomersWaiting() {
+            return customersQueued.get();
         }
 
     }
