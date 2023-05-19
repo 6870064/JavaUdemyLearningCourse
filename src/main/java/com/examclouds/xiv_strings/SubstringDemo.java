43e598086e733b66959b914c952eca444ab74525
6870064@gmail.com <mewqrty554e$%@>
2023-05-19 13:15:11 +0200

    Collections and Strings

---
+++ src/main/java/com/examclouds/xiv_strings/SubstringDemo.java
@@ -0,0 +1,11 @@
+package com.examclouds.xiv_strings;
+
+public class SubstringDemo {
+    public static void main(String[] args) {
+        String str = "Welcome to Kharkiv!!!";
+        System.out.println(str.substring(10));
+        System.out.println(str.substring(10, 18));
+
+
+    }
+}
