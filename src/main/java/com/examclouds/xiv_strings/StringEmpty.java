b3fa11ddca2319d811e9e918596f968c54e1ebd1
6870064@gmail.com <mewqrty554e$%@>
2023-05-19 13:30:08 +0200

    Strings methods

---
+++ src/main/java/com/examclouds/xiv_strings/StringEmpty.java
@@ -0,0 +1,11 @@
+package com.examclouds.xiv_strings;
+
+public class StringEmpty {
+    public static void main(String[] args) {
+        String str1 = "";
+        String str2 = " ";
+        System.out.println(str1.isEmpty());
+        System.out.println(str2.isEmpty());
+        System.out.println(str2.isBlank());
+    }
+}
---
+++ src/main/java/com/examclouds/xiv_strings/StringJoinDemo.java
@@ -0,0 +1,8 @@
+package com.examclouds.xiv_strings;
+
+public class StringJoinDemo {
+    public static void main(String[] args) {
+        String result = String.join(":", "foobar", "foo", "bar");
+        System.out.println(result);
+    }
+}
