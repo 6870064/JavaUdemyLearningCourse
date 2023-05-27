fca488f046b3bb908e1230fffbcfd22449434238
Dzmitry Viachaslavau <6870064@gmail.com>
2023-05-27 15:01:01 +0200

    Strings methods

---
+++ src/main/java/com/examclouds/xiv_strings/SimpleFormatString.java
@@ -0,0 +1,11 @@
+package com.examclouds.xiv_strings;
+
+import java.util.Formatter;
+
+public class SimpleFormatString {
+    public static void main(String[] args) {
+        Formatter f = new Formatter();
+        f.format("This %s is about %n%S %c", "book", "java", '8');
+        System.out.println(f);
+    }
+}
