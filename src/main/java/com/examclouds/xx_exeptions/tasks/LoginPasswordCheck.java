a74307f10fc37fa1a02c5a018dc8c2aeb008a3d2
6870064@gmail.com <mewqrty554e$%@>
2023-10-10 12:47:59 +0200

    Exceptions task

--- src/main/java/com/examclouds/xx_exeptions/tasks/LoginPasswordCheck.java
+++ src/main/java/com/examclouds/xx_exeptions/tasks/LoginPasswordCheck.java
@@ -1,6 +1,43 @@
 package com.examclouds.xx_exeptions.tasks;
 
 public class LoginPasswordCheck {
+
+    public static void main(String[] args) {
+        checkLoginResult("my_login123", "my_password123", "my_password123");
+    }
+
+    public static boolean checkLoginResult(String login, String password, String confirmPassword) {
+        boolean result = checkLoginAndPassword(login, password, confirmPassword);
+
+        if (result) {
+            System.out.println("Login and password are correct");
+        } else {
+            System.out.println("Login and password are wrong");
+        }
+        return result;
+    }
+
+    public static boolean checkLoginAndPassword(String login, String password, String confirmPassword) {
+
+        try {
+            //Проверяем login
+            if (!login.matches("^[a-aA-AZ0-9_]{1,20}$")) {
+                throw new WrongLoginExeption("Login is wrong");
+            }
+
+            //Проверяем password
+            if (!password.equals(confirmPassword)) {
+                throw new WrongPasswordException("Passwords do not match");
+            }
+            if (!password.matches("^[a-zA-Z0-9_]{1,20}$")) {
+                throw new WrongPasswordException();
+            }
+            return true;
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
 
 
