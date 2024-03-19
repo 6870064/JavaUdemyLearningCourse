85ab93dfd1f4b8b2c637ec6979e4c0c0885a0aa0
6870064@gmail.com <mewqrty554e$%@>
2024-03-19 17:14:33 +0100

    Задача 3. Найдите факториал заданного числа. Правки

--- src/main/java/com/tasks_for_beginners/Task3.java
+++ src/main/java/com/tasks_for_beginners/Task3.java
@@ -7,12 +7,15 @@ toFindFactorial(10);
 
     public static void toFindFactorial(int a) {
 
-        int factorial = 1;
+        int factorial = 1; // Изначально присваиваем факториалу значение 1
+
+        // Используем цикл for для нахождения факториала
         for (int i = 1; i <= a; i++) {
 
-            factorial = factorial * i;
+            factorial *= i; // Умножаем текущее значение факториала на текущее значение i
             System.out.println(factorial+ " " +i);
         }
+        // Выводим результат
         System.out.println("Factorial of " +a+ " is equal " +factorial);
     }
 }
